ace0ef082ace003e95d96bd131d40b56a010e78d
Anwar Hossain <anwarhossain221095@gmail.com>
2020-08-24 23:27:41 +0600

    remove duplicate route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'product/:id', component: ProductDetailsComponent}
 ];
